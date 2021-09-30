d61d6449d7dc7ddf619485c4c6e6f0b98965ffd7
Shashi Srinath <atshashisrinath@gmail.com>
2021-09-30 12:52:09 +0530

    create sample questions

--- src/app/types.ts
+++ src/app/types.ts
@@ -2,15 +2,15 @@ export type Question = {
   type: 'text'
   question: string;
   correctAnswer: string;
-  answered: boolean;
-  answer: string;
+  answered?: boolean;
+  answer?: string;
 } | {
   type: 'mcq'
   question: string;
   choices: string[]
   correctAnswer: string;
-  answered: boolean;
-  answer: string;
+  answered?: boolean;
+  answer?: string;
 }
 
 export type Result = {
