c82b17d5d4299e70c5c7d497d7f1899a20795ad8
Shashi Srinath <atshashisrinath@gmail.com>
2021-09-30 11:38:13 +0530

    create questions array

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import {Question} from "./types";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppService {
+  questions: Question[];
+
 
   constructor() { }
 }
