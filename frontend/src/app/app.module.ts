3a11bf38a20a92471fb12db081cc7fe0a8543ea3
Sanjula Premasiri <sanjulapremasiri3@gmail.com>
2021-10-04 10:21:00 +0530

    change quiz upload

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -13,7 +13,6 @@ import {FormsModule} from "@angular/forms";
 import { QuizJoinComponent } from './quiz-join/quiz-join.component';
 import { MainFooterComponent } from './main-footer/main-footer.component';
 import { HomeComponent } from './home/home.component';
-import { MainFooterComponent } from './main-footer/main-footer.component';
 import { QuizUploadComponent } from './quiz-upload/quiz-upload.component';
 
 @NgModule({
