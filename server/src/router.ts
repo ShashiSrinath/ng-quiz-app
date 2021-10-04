accfe9ec6a6b5b591239defd386a67cc299c4ffa
Shevan Fernando <81354288+shevanfernando@users.noreply.github.com>
2021-10-04 07:48:03 +0530

    merge

--- server/src/router.ts
+++ server/src/router.ts
@@ -2,6 +2,9 @@ import { Router } from 'express';
 import authRouter from './api/auth/auth.router';
 import quizRouter from './api/quiz/quiz.router';
 import answerSheetRouter from './api/answer-sheet/answer-sheet.route';
+=======
+import quizRouter from './api/quiz/quiz.router';
+>>>>>>> ffe805dabbf8aa9d427d816cac1c44c0c885f51e
 
 const router = Router();
 const API_PREFIX = '/api';
