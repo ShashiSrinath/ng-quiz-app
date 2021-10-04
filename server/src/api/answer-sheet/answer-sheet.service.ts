a20f9ae2c5e6c7554ea431cdf3c423ce0f785433
Shevan Fernando <w.k.b.s.t.fernando@gmail.com>
2021-10-04 13:01:39 +0530

    add validations to submit single answer method

--- server/src/api/answer-sheet/answer-sheet.service.ts
+++ server/src/api/answer-sheet/answer-sheet.service.ts
@@ -1,16 +1,40 @@
 import { AnswerSheetModel } from './answer-sheet.model';
 import { CreateAnswerSheetDTO } from './dto/create-answer-sheet.dto';
 import { SubmitSingleAnswerDTO } from './dto/submit-single-answer.dto';
+import { QuestionModel } from '../question/question.model';
+import { QuizModel } from '../quiz/quiz.model';
+import { HttpError } from '../../lib/http-error';
 
 async function createAnswerSheet(data: CreateAnswerSheetDTO) {
     return AnswerSheetModel.create({ ...data, status: 'Ongoing' });
 }
 
 async function submitSingleAnswer(data: SubmitSingleAnswerDTO) {
-    const { answers } = await AnswerSheetModel.findById(data._id);
-    return AnswerSheetModel.findByIdAndUpdate(data._id, {
-        answers: { ...answers, ...data.answers },
+    const answerSheet = await AnswerSheetModel.findById(data._id);
+
+    if (answerSheet.status === 'Finished') {
+        throw new HttpError(400, 'answer sheet already submitted');
+    }
+
+    const quiz = await QuizModel.findOne({
+        _id: answerSheet.quizId,
+    }).populate('questions');
+
+    if (
+        !quiz.questions.get(data.questionNumber.toString()) ||
+        quiz.questions.get(data.questionNumber.toString()).id.toString() !==
+            data.questionId
+    ) {
+        throw new HttpError(400, 'invalid question id');
+    }
+
+    const question = await QuestionModel.findById(data.questionId);
+    answerSheet.answers.set(question.questionNumber.toString(), {
+        question: data.questionId,
+        answer: data.answer,
     });
+
+    return answerSheet.save();
 }
 
 async function finishAnswerSheet(_id: string) {
