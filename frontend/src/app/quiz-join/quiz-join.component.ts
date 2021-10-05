ee8c96ecef98d9d23c8cf498dffa682afb619bde
Shashi Srinath <atshashisrinath@gmail.com>
2021-10-05 14:35:51 +0530

    add quiz id
    add quiz id to the quiz attend page and setup ng reactive forms

--- frontend/src/app/quiz-join/quiz-join.component.ts
+++ frontend/src/app/quiz-join/quiz-join.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-quiz-join',
   templateUrl: './quiz-join.component.html',
-  styleUrls: ['./quiz-join.component.css']
+  styleUrls: ['./quiz-join.component.css'],
 })
 export class QuizJoinComponent implements OnInit {
+  attendForm = new FormGroup({
+    quizId: new FormControl('', Validators.required),
+    username: new FormControl('', Validators.required),
+    passcode: new FormControl('', Validators.required),
+  });
 
-  constructor() { }
+  constructor(private apiService: ApiService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  attendQuiz(): void {
+    if (this.attendForm.valid) {
+    }
   }
-
 }
