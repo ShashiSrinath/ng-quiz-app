9f5c5c4d1fe5b2bbc52064825ad7c42adbce2729
Shashi Srinath <atshashisrinath@gmail.com>
2021-10-06 12:03:10 +0530

    add routing to do the quiz workflow

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -5,16 +5,18 @@ import { AuthComponent } from './auth/auth.component';
 import { HomeComponent } from './home/home.component';
 import { QuizJoinComponent } from './quiz-join/quiz-join.component';
 import { QuizUploadComponent } from './quiz-upload/quiz-upload.component';
+import { AllQuizComponent } from './all-quiz/all-quiz.component';
 
 const routes: Routes = [
-{path:"", component:HomeComponent},
-{path:"attend", component:QuizJoinComponent},
-{path: "upload", component: QuizUploadComponent, canActivate: [AuthGuard]},
-{path: "auth", component: AuthComponent}
+  { path: '', component: HomeComponent },
+  { path: 'attend', component: QuizJoinComponent },
+  { path: 'upload', component: QuizUploadComponent, canActivate: [AuthGuard] },
+  { path: 'auth', component: AuthComponent },
+  { path: 'quiz/:id', component: AllQuizComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
