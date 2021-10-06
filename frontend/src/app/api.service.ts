2098757a514687e45e1edaf4cdb0140c4f42d9d1
Shashi Srinath <atshashisrinath@gmail.com>
2021-10-06 12:02:20 +0530

    add required api services
    add api required api services to do the attend quiz and create quiz workflow

--- frontend/src/app/api.service.ts
+++ frontend/src/app/api.service.ts
@@ -1,43 +1,79 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { AppService } from './app.service';
+import { AnswerSheet, Quiz, Result } from './types';
 
-const live_server = "http://localhost:3000/api/";
+const live_server = 'http://localhost:3000/api/';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
-
-  constructor(private http: HttpClient, private appService: AppService) { 
-
-    this.checkAuth().subscribe(
-      data => {
-        appService.user = data.id;
-      }
-    );
+  constructor(private http: HttpClient, private appService: AppService) {
+    this.checkAuth().subscribe((data) => {
+      appService.user = data.id;
+    });
   }
 
   // login user
   public login(email: string, password: string) {
-    let url = live_server + "auth/login";
-    return this.http.post<{id: string}>(url, {email:email, password: password}, {withCredentials: true});
+    let url = live_server + 'auth/login';
+    return this.http.post<{ id: string }>(
+      url,
+      { email: email, password: password },
+      { withCredentials: true }
+    );
   }
 
   // register user
   public register(email: string, password: string) {
-    let url = live_server + "auth/register";
-    return this.http.post<{id: string}>(url, {email: email, password: password}, {withCredentials: true});
+    let url = live_server + 'auth/register';
+    return this.http.post<{ id: string }>(
+      url,
+      { email: email, password: password },
+      { withCredentials: true }
+    );
   }
 
   // create quiz
   public createQuiz(file: FormData) {
-    let url = live_server + "quiz/create-from-xlsx";
-    return this.http.post(url, file, {withCredentials: true});
+    let url = live_server + 'quiz/create-from-xlsx';
+    return this.http.post(url, file, { withCredentials: true });
   }
 
   public checkAuth() {
-    let url = live_server + "auth/check";
-    return this.http.get<{id: string}>(url, {withCredentials: true});
+    let url = live_server + 'auth/check';
+    return this.http.get<{ id: string }>(url, { withCredentials: true });
+  }
+
+  public attendQuiz(data: {
+    quizId: string;
+    passcode: string;
+    userCode: string;
+  }) {
+    let url = live_server + 'answer-sheet/create-answer-sheet';
+    return this.http.post<AnswerSheet>(url, data, { withCredentials: true });
+  }
+
+  public fetchQuizAsStudent(quizId: string) {
+    let url = live_server + 'quiz/as-student/' + quizId;
+    return this.http.get<Quiz>(url, { withCredentials: true });
+  }
+
+  public updateSingleAnswer(
+    questionId: string,
+    questionNumber: number,
+    answer: string
+  ) {
+    let url = live_server + 'answer-sheet/submit-single-answer';
+    return this.http.post(
+      url,
+      { questionId, questionNumber, answer },
+      { withCredentials: true }
+    );
+  }
+  public submitAnswerSheet() {
+    let url = live_server + 'answer-sheet/finish-answer-sheet';
+    return this.http.post<Result>(url, {}, { withCredentials: true });
   }
 }
