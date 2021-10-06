f5bb5636e548743cfeae977dc57eb3a04a0e6fbc
Shashi Srinath <atshashisrinath@gmail.com>
2021-10-06 12:06:16 +0530

    add redirect to quiz join path

--- frontend/src/app/quiz-join/quiz-join.component.ts
+++ frontend/src/app/quiz-join/quiz-join.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ApiService } from '../api.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-quiz-join',
@@ -8,18 +9,35 @@ import { ApiService } from '../api.service';
   styleUrls: ['./quiz-join.component.css'],
 })
 export class QuizJoinComponent implements OnInit {
+  error?: string;
+
   attendForm = new FormGroup({
-    quizId: new FormControl('', Validators.required),
-    username: new FormControl('', Validators.required),
-    passcode: new FormControl('', Validators.required),
+    quizId: new FormControl(undefined, Validators.required),
+    userCode: new FormControl(undefined, Validators.required),
+    passcode: new FormControl(undefined, Validators.required),
   });
 
-  constructor(private apiService: ApiService) {}
+  constructor(private apiService: ApiService, private router: Router) {}
 
   ngOnInit(): void {}
 
   attendQuiz(): void {
+    this.error = undefined;
+    this.attendForm.markAsDirty();
     if (this.attendForm.valid) {
+      this.apiService.attendQuiz(this.attendForm.value).subscribe(
+        (data) => {
+          this.router.navigate([`quiz/${data.quizId}`]);
+        },
+        (err) => {
+          if (err.error.name === 'ValidationError') {
+            this.error = err.error.details[0].message;
+          }
+          console.log(err);
+        }
+      );
+    } else {
+      this.error = 'Please fill the required fields';
     }
   }
 }
