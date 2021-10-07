77d17d620e26564c7a7d111f55e24cafbbd8496a
Sanjula Premasiri <83264643+sanjulap@users.noreply.github.com>
2021-10-07 10:52:01 +0530

    change auth page interface

--- frontend/src/app/auth/auth.component.ts
+++ frontend/src/app/auth/auth.component.ts
@@ -11,12 +11,14 @@ import { AppService } from '../app.service';
 export class AuthComponent implements OnInit {
   email: string = "";
   password: string = "";
+  isOpenRegisterForm: boolean = false;
 
   constructor(private apiService: ApiService, private appService: AppService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  // call login API
   login() {
     this.apiService.login(this.email, this.password).subscribe(
       data => {
@@ -24,21 +26,32 @@ export class AuthComponent implements OnInit {
         this.router.navigate(["dashboard"]);
         console.log(data);
       }, error => {
-        console.log("error");
+        console.log(error);
       }
     );
   }
 
+  // call register API
   register() {
     this.apiService.register(this.email, this.password).subscribe(
       data => {
         this.appService.user = data.id;
         console.log(data);
+        this.isOpenRegisterForm = false;
         console.log("register successfull");
       }, error => {
         console.log("register error");
       }
     );
+  }
+
+  // switch among login and register form
+  switchForms(switchTo: string) {
+    if (switchTo === "register") {
+      this.isOpenRegisterForm = true;
+    } else {
+      this.isOpenRegisterForm = false;
+    }
   }
 
 }
